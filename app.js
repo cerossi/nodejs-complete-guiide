0e115554f810f1092ced8e4b74288712abe896d2
cerossi <cesare.eiras@gmail.com>
2025-04-16 11:25:11 -0300

    Removing pug and html files

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-// app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
